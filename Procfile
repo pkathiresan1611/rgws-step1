2d2f40a56bbb91acfc0ab8afc9c9f70d13d8f7c6
pkathiresan1611 <kathirrgws@gmail.com>
2018-08-03 04:41:52 +0530

    modified procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn gettingstarted.wsgi --log-file -
+web: gunicorn rayGlobe.wsgi --log-file -
--- rayGlobe/settings.py
+++ rayGlobe/settings.py
@@ -130,6 +130,12 @@ STATICFILES_DIRS = os.path.join((BASE_DIR), 'static')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+try:
+    from production_settings import *
+except ImportError:
+    pass
+
+# try:
+#     from local_settings import *
+# except ImportError:
+#     pass
